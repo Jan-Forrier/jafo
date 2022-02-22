f57d8c6e08420ecc68460198f14f30437c3e3ad8
Jan-Forrier <83003411+Jan-Forrier@users.noreply.github.com>
2022-02-22 21:46:04 +0100

    adding pasmic and plasmic template page

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,5 +23,19 @@ module.exports = {
             token: 'skNuqd63tD8ZA80UUbBp9Si901BuEzHyOP5IrwPN9sElguR9oTFsjCTvvxPdxAUYBgtjBwugoCnnNttdK7y3xNjHPSIp59rDPh1haY9GObv4MafdJG7va8J33IbXJeCODF9Na2blX0ZgmQLAYNk7yckXk7GY0gywgduclGVVZMRniAQb5rUn',
           },
         },
+        {
+          resolve: '@plasmicapp/loader-gatsby',
+          options: {
+            projects: [
+              {
+                id: 'gq3Uq8Ndyp9bFhPCbiKgyk', // ID of a project you are using
+                token: 'MmI40HzpH5KT6dtd0SMZNpPq55hXq5MsHOhTN7tx2FlkDmiysUCKvHtD6iyf6CrzmmvVuMEt7zQ4f1IKHo0Q' // API token for that project
+              }
+            ],
+            defaultPlasmicPage: require.resolve('./src/templates/defaultPlasmicPage.tsx'),
+            // Optionally specify pages to ignore.
+            ignorePaths: ['/my-page']
+          }
+        },
     ]
 }
